42daa3f65fa2cc98de38fd2a2aab2fa642e23232
Skye <69115669+Lisenaaaa@users.noreply.github.com>
2022-06-14 15:52:20 -0700

    ok i'm just stupid

--- autoinstall_arch.sh
+++ autoinstall_arch.sh
@@ -13,7 +13,7 @@ paru -S awesome-git rofi-git picom-jonaburg-git ttf-meslo-nerd-font-powerlevel10
 sudo pacman -S papirus-icon-theme pulseaudio-alsa upower bluez bluez-utils xorg-setxkbmap xfce4-power-manager playerctl lightdm light-locker alacritty thunar flameshot
 
 cd
-git clone --recurse-submodules https://github.com/Crylia/crylia-theme
+git clone --recurse-submodules https://github.com/Lisenaaaa/crylia-theme
 cd crylia-theme
 cp -r awesome ~/.config/.
 cp -r picom.conf ~/.config/.
